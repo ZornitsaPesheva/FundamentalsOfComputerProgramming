aea80bfb92844aec15f3b1e4dd3d026c0a2b6abf
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-24 14:51:41 +0300

    Arrays

---
+++ 7.Arrays/3.LexographicalCompare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.LexographicalCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr1 = { 'a', 'f', 'h' };
+            char[] arr2 = { 'a', 'f', 'c' };
+            int length = Math.Min(arr1.Length, arr2.Length);
+            string first = "equal";
+            for (int i = 0; i < length; i++)
+            {
+                if ((int)arr1[i] < (int)arr2[i])
+                {
+                    first = "arr1";
+                }
+                else if ((int)arr1[i] > (int)arr2[i])
+                {
+                    first = "arr2";
+                }
+                if (first != "equal")
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(first);
+        }
+    }
+}
