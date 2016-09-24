24b2202f470fc44a88bfc83bfd73494d55474aee
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-24 15:13:39 +0300

    arrays

---
+++ 7.Arrays/4.MaxSubArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.MaxSubArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 6, 6, 3, 3, 3, 4, 4, 4, 4, 5, 5 };
+            int currentStart = 0;
+            int currentLenght = 1;
+            int longestStart = 0;
+            int longestLenght = 1;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    currentLenght++;
+                    if (currentLenght > longestLenght)
+                    {
+                        longestLenght = currentLenght;
+                        longestStart = currentStart;
+                    }
+                }
+                else
+                {
+                    currentStart = i;
+                    currentLenght = 1;
+                }
+            }
+            Console.WriteLine($"form {longestStart} have {longestLenght} equal symbols");
+        }
+    }
+}
