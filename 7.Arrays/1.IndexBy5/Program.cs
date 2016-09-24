f869f0d900c42bb93a86b4d7de09cfd8d33812a1
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-24 14:08:08 +0300

    arrays

--- 7.Arrays/1.IndexBy5/Program.cs
+++ 7.Arrays/1.IndexBy5/Program.cs
@@ -11,7 +11,7 @@ namespace _1.IndexBy5
         static void Main(string[] args)
         {
             int[] array = new int[20];
-            for (int i = 0; i < array.Length -1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array[i] = i * 5;
                 Console.WriteLine(array[i]);
---
+++ 7.Arrays/2.TwoEqualArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.TwoEqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+            bool equals = true;
+            for (int i = 0; i < 10; i++)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                array2[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    equals = false;
+                }
+            }
+            Console.WriteLine(equals);
+
+        }
+    }
+}
