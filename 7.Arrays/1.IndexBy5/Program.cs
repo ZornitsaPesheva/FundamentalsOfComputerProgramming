020aa666333c468edbecdeaed3fff4503eb9641f
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-24 13:59:08 +0300

    Arrays

---
+++ 7.Arrays/1.IndexBy5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.IndexBy5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                array[i] = i * 5;
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
