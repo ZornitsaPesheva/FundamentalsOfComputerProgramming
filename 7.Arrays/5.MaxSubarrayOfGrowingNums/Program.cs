af1ae9b71dd690b55323adfd79be24f0e9b2ad9c
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-25 15:58:52 +0300

    Arrays

--- 7.Arrays/5.MaxSubarrayOfGrowingNums/Program.cs
+++ 7.Arrays/5.MaxSubarrayOfGrowingNums/Program.cs
@@ -4,20 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _4.MaxSubArray
+namespace _5.MaxSubarrayOfGrowingNums
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 6, 6, 3, 3, 3, 4, 4, 4, 4, 5, 5 };
+            int[] array = { 1, 2, 6, 2, 3, 4, 3, 4, 5, 6, 4, 5, 6 };
             int currentStart = 0;
             int currentLenght = 1;
             int longestStart = 0;
             int longestLenght = 1;
             for (int i = 1; i < array.Length; i++)
             {
-                if (array[i] == array[i - 1])
+                if (array[i] > array[i - 1])
                 {
                     currentLenght++;
                     if (currentLenght > longestLenght)
---
+++ 7.Arrays/6.MaxArrayGrowngNotInSiquence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.MaxArrayGrowngNotInSiquence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] array = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4 };
+            int[] len = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] 
+            }
+        }
+    }
+}
