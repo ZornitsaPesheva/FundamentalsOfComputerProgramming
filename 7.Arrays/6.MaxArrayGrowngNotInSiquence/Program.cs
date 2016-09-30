d5479ca898f59bd7762d07f791564e2edab5f825
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-09-30 11:12:10 +0300

    Fundamentals

--- 7.Arrays/6.MaxArrayGrowngNotInSiquence/Program.cs
+++ 7.Arrays/6.MaxArrayGrowngNotInSiquence/Program.cs
@@ -11,11 +11,34 @@ namespace _6.MaxArrayGrowngNotInSiquence
         static void Main(string[] args)
         {
             
-            int[] array = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4 };
-            int[] len = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-            for (int i = 1; i < array.Length; i++)
+            int[] arr = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 9 };
+            int[] len = new int[10];
+            for (int x = 0; x < arr.Length; x++)
             {
-                array[i] 
+                int prev = 0;
+                for (int i = 0; i <= x; i++)
+                {
+                    if (arr[x] > arr[i])
+                    {
+                        prev = i;
+                    }
+                }
+                if (prev != 0)
+                {
+                    len[x] = 1 + len[prev];
+                }
+                else
+                {
+                    len[x] = 1;
+                }
+            }
+            if (arr[arr.Length - 1] > arr[arr.Length - 2])
+            {
+                len[arr.Length - 1] = len[arr.Length - 2];
+            }
+            for (int i = 0; i < len.Length - 1; i++)
+            {
+                 Console.WriteLine(len[i]);
             }
         }
     }
